857546c0cc724f91ee24ae1915a200c8df7b85da
mitswap <susmitdebnath326@gmail.com>
2026-06-25 01:19:05 +0600

    Initial Commit

---
+++ Ironvault-backend/src/main/java/com/ironvault/banking/IronVaultApplication.java
@@ -0,0 +1,12 @@
+package com.ironvault.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IronVaultApplication.class, args);
+    }
+}
